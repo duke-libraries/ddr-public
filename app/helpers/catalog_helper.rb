bb2c2647873a0b55b04e6af6afc62f1275132d10
Jim Coble <jim.coble@duke.edu>
2014-10-27 16:41:08 -0400

    add tests for thumbnail helpers; closes #3

--- app/helpers/catalog_helper.rb
+++
@@ -1,9 +0,0 @@
-module CatalogHelper
-  include Blacklight::CatalogHelperBehavior
-
-  def thumbnail_image_tag document, image_options = {}
-    src = document.has_thumbnail? ? thumbnail_path(document) : default_thumbnail(document)
-    thumbnail = image_tag(src, :alt => "Thumbnail", :class => "img-thumbnail")
-  end
-
-end
