b90186b5018e1b505cebc52409e1427aae397382
Jim Coble <jim.coble@duke.edu>
2014-10-28 11:53:05 -0400

    set index display fields; provide sample secrets file; closes #5

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -43,13 +43,13 @@ class CatalogController < ApplicationController
     #
     # :show may be set to false if you don't want the facet to be drawn in the
     # facet bar
-    config.add_facet_field solr_name('object_type', :facetable), :label => 'Format'
-    config.add_facet_field solr_name('pub_date', :facetable), :label => 'Publication Year'
-    config.add_facet_field solr_name('subject_topic', :facetable), :label => 'Topic', :limit => 20
-    config.add_facet_field solr_name('language', :facetable), :label => 'Language', :limit => true
-    config.add_facet_field solr_name('lc1_letter', :facetable), :label => 'Call Number'
-    config.add_facet_field solr_name('subject_geo', :facetable), :label => 'Region'
-    config.add_facet_field solr_name('subject_era', :facetable), :label => 'Era'
+    # config.add_facet_field solr_name('object_type', :facetable), :label => 'Format'
+    # config.add_facet_field solr_name('pub_date', :facetable), :label => 'Publication Year'
+    # config.add_facet_field solr_name('subject_topic', :facetable), :label => 'Topic', :limit => 20
+    # config.add_facet_field solr_name('language', :facetable), :label => 'Language', :limit => true
+    # config.add_facet_field solr_name('lc1_letter', :facetable), :label => 'Call Number'
+    # config.add_facet_field solr_name('subject_geo', :facetable), :label => 'Region'
+    # config.add_facet_field solr_name('subject_era', :facetable), :label => 'Era'
 
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
@@ -61,15 +61,9 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field solr_name('title', :stored_searchable, type: :string), :label => 'Title:'
-    config.add_index_field solr_name('title_vern', :stored_searchable, type: :string), :label => 'Title:'
-    config.add_index_field solr_name('author', :stored_searchable, type: :string), :label => 'Author:'
-    config.add_index_field solr_name('author_vern', :stored_searchable, type: :string), :label => 'Author:'
-    config.add_index_field solr_name('format', :symbol), :label => 'Format:'
-    config.add_index_field solr_name('language', :stored_searchable, type: :string), :label => 'Language:'
-    config.add_index_field solr_name('published', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_index_field solr_name('published_vern', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_index_field solr_name('lc_callnum', :stored_searchable, type: :string), :label => 'Call number:'
+    config.add_index_field solr_name(:creator, :stored_searchable), :label => 'Creator'
+    config.add_index_field solr_name(:date, :stored_searchable), :label => 'Date'
+    config.add_index_field solr_name(:type, :stored_searchable), :label => 'Type'
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
