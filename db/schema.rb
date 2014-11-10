3fe8a3f4863ce42533ab6434aeb908a1141a98de
Jim Coble <jim.coble@duke.edu>
2014-11-10 13:05:11 -0500

    remove devise guest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110151619) do
+ActiveRecord::Schema.define(version: 20141110175405) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",       null: false
@@ -65,20 +65,19 @@ ActiveRecord::Schema.define(version: 20141110151619) do
   add_index "searches", ["user_id"], name: "index_searches_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "guest",                  default: false
-    t.string   "username",               default: "",    null: false
+    t.string   "username",               default: "", null: false
     t.string   "first_name"
     t.string   "middle_name"
     t.string   "nickname"
