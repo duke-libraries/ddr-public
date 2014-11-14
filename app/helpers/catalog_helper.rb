874f7434336441bfac03f0ff12a8ac38856babd7
Jim Coble <jim.coble@duke.edu>
2014-11-14 15:29:22 -0500

    fix problem based on indexing change

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -7,7 +7,7 @@ module CatalogHelper
 
   def find_children document, relationship
     configure_blacklight_for_children
-    query = ActiveFedora::SolrService.construct_query_for_rel([[relationship, document[Ddr::IndexFields::INTERNAL_URI].first]])
+    query = ActiveFedora::SolrService.construct_query_for_rel([[relationship, document[Ddr::IndexFields::INTERNAL_URI]]])
     @response, @document_list = get_search_results(params, {q: query})
   end
 
