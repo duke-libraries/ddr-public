e6d5e58391ebe36dd21f4cf3df5be7fe1012bd3e
Jim Coble <jim.coble@duke.edu>
2014-11-19 16:13:17 -0500

    clean up migrations

--- db/migrate/20141021171735_create_bookmarks.blacklight.rb
+++ db/migrate/20141021171735_create_bookmarks.blacklight.rb
@@ -2,17 +2,19 @@
 # -*- encoding : utf-8 -*-
 class CreateBookmarks < ActiveRecord::Migration
   def self.up
-    create_table :bookmarks do |t|
-      t.integer :user_id, :null=>false
-      t.string :user_type
-      t.string :document_id
-      t.string :title
-      t.timestamps
+    unless table_exists?(:bookmarks)
+      create_table :bookmarks do |t|
+        t.integer :user_id, :null=>false
+        t.string :user_type
+        t.string :document_id
+        t.string :title
+        t.timestamps
+      end
     end
   end
 
   def self.down
-    drop_table :bookmarks
+    raise ActiveRecord::IrreversibleMigration
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -70,13 +70,13 @@ ActiveRecord::Schema.define(version: 20141110175405) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "username",               default: "", null: false
     t.string   "first_name"
     t.string   "middle_name"
