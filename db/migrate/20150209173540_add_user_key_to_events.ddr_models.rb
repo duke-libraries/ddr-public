17311d404439e9153445153ca16ba0d8640269b1
Jim Coble <jim.coble@duke.edu>
2015-02-10 11:13:49 -0500

    remove duplicated migration

--- db/migrate/20150209173540_add_user_key_to_events.ddr_models.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from ddr_models (originally 20150130134416)
-class AddUserKeyToEvents < ActiveRecord::Migration
-  def change
-    change_table :events do |t|
-      t.string :user_key
-    end
-  end
-end
