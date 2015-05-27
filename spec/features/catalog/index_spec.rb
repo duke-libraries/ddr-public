317fb3044cdb21700860f3d88df08bd97264030f
Jim Coble <jim.coble@duke.edu>
2015-05-27 14:11:20 -0400

    Remove collection facet in favor of admin set facet; closes #114

--- spec/features/catalog/index_spec.rb
+++ spec/features/catalog/index_spec.rb
@@ -3,14 +3,18 @@ require 'rails_helper'
 RSpec.describe "catalog/index", :type => :feature do
 
   describe "faceting" do
-    let(:published_collection_c) { Collection.create(title: ["Collection C"], read_groups: ["public"]) }
+    let(:published_collection_c) { Collection.create(title: ["Collection C"], read_groups: ["public"], admin_set: 'ghi') }
     let(:published_item_1) { Item.create(title: ["Item 1"]) }
     let(:published_item_2) { Item.create(title: ["Item 2"]) }
-    let(:unpublished_collection) { Collection.create(title: ["Collection B"], read_groups: ["public"]) }
+    let(:unpublished_collection) { Collection.create(title: ["Collection B"], read_groups: ["public"], admin_set: 'def') }
     let(:unpublished_item) { Item.create(title: ["Item 3"]) }
-    let(:published_collection_a) { Collection.create(title: ["Collection A"], read_groups: ["public"]) }
+    let(:published_collection_a) { Collection.create(title: ["Collection A"], read_groups: ["public"], admin_set: 'abc') }
     let(:published_item_4) { Item.create(title: ["Item 4"]) }
     before do
+      allow(I18n).to receive(:t).and_call_original
+      allow(I18n).to receive(:t).with('ddr.admin_set.abc') { 'XYZ Collection Group' }
+      allow(I18n).to receive(:t).with('ddr.admin_set.def') { 'UVW Collection Group' }
+      allow(I18n).to receive(:t).with('ddr.admin_set.ghi') { 'RST Collection Group' }
       published_collection_c.default_permissions = [ {:name=>"public", :access=>"read", :type=>"group"} ]
       published_item_1.admin_policy = published_collection_c
       published_item_2.admin_policy = published_collection_c
@@ -34,10 +38,10 @@ RSpec.describe "catalog/index", :type => :feature do
       published_collection_a.publish!
       published_item_4.publish!
     end
-    it "should display the collection facet with the published collections in title order" do
+    it "should display the admin set facet with the admin sets containing published collections in title order" do
       visit catalog_index_path
-      expect(published_collection_a.title_display).to appear_before(published_collection_c.title_display)
-      expect(page).to_not have_link("Collection B")
+      expect("RST Collection Group").to appear_before("XYZ Collection Group")
+      expect(page).to_not have_link("UVW Collection Group")
     end
   end
 
