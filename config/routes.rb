aabc9e427cec2a0d5fe1fe0a6bb50b2e738631cb
Cory Lown <cory.lown@duke.edu>
2016-02-08 11:16:58 -0500

    Constrain collection portal routes to configured portals

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,20 @@ Rails.application.routes.draw do
 
   get "dc/range_limit/", to: "digital_collections#range_limit"
   get "dc/facet/:id", to: "digital_collections#facet", as: "digital_collections_facet"
-  get "dc/:collection/featured", to: "digital_collections#featured", as: "featured_items"
-  get "dc/:collection/about", :to => "digital_collections#about", as: "digital_collections_about"
-  get "dc/:collection/:id/media", :to => "digital_collections#media"
-  get "dc/:collection/:id", to: "digital_collections#show"
-  get "dc/:collection", to: "digital_collections#index", as: "digital_collections"
+  constraints Collection do
+    get "dc/:collection/featured", to: "digital_collections#featured", as: "featured_items"
+    get "dc/:collection/about", :to => "digital_collections#about", as: "digital_collections_about"
+    get "dc/:collection/:id/media", :to => "digital_collections#media"
+    get "dc/:collection/:id", to: "digital_collections#show"
+    get "dc/:collection", to: "digital_collections#index", as: "digital_collections"
+  end
   get "dc" => "digital_collections#index"
+
+  class Collection
+    def self.matches?(request)
+      Rails.application.config.portal.try(:[], 'portals').try(:[], 'collection_local_id').keys.include?(request.params[:collection])
+    end
+  end
 
   def pid_constraint
       /[a-zA-Z0-9\-_]+:[a-zA-Z0-9\-_]+/
