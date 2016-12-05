ba4e72985638993b153eb2a3b329816433f1f7aa
Cory Lown <cory.lown@duke.edu>
2016-12-05 13:45:31 -0500

    Use fedora 3 field names

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -94,8 +94,8 @@ class CatalogController < ApplicationController
     config.add_index_field solr_name(:type, :stored_searchable), separator: '; ', label:'Type'
     config.add_index_field Ddr::Index::Fields::PERMANENT_URL.to_s, helper_method: 'permalink', label: 'Permalink'
     config.add_index_field Ddr::Index::Fields::MEDIA_TYPE.to_s, helper_method: 'file_info', label: 'File'
-    config.add_index_field :isPartOf_ssim, helper_method: 'descendant_of', label: 'Part of'
-    config.add_index_field :isMemberOfCollection_ssim, helper_method: 'descendant_of', label: 'Collection'
+    config.add_index_field Ddr::Index::Fields::IS_PART_OF.to_s, helper_method: 'descendant_of', label: 'Part of'
+    config.add_index_field Ddr::Index::Fields::IS_MEMBER_OF_COLLECTION.to_s, helper_method: 'descendant_of', label: 'Collection'
     config.add_index_field Ddr::Index::Fields::COLLECTION_URI.to_s, helper_method: 'descendant_of', label: 'Collection'
 
     config.default_document_solr_params = {
