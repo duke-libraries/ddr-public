33c7c96760506a73689609b399815b1181fdb84e
Cory Lown <cory.lown@duke.edu>
2017-02-08 17:27:56 -0500

    Add support for isFormatOf item relationships

--- spec/models/related_item/shared_value_spec.rb
+++ spec/models/related_item/shared_value_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe RelatedItem do
+RSpec.describe RelatedItem::SharedValue do
 
   let(:doc1) { SolrDocument.new('id' => 'changeme:1', 'title' => 'A') }
   let(:doc2) { SolrDocument.new('id' => 'changeme:2', 'title' => 'B') }
@@ -11,14 +11,12 @@ RSpec.describe RelatedItem do
 
   subject { described_class.new({document: doc1, config: config}) }
 
-    before do
-      allow(subject).to receive(:facet_values_and_counts) { facet_vals_counts }
-      allow(subject).to receive(:relator_field_query) { docs_list }
-    end
+  before do
+    allow(subject).to receive(:facet_values_and_counts) { facet_vals_counts }
+    allow(subject).to receive(:relator_field_query) { docs_list }
+  end
 
   its(:name) { is_expected.to eq("Related Items") }
-  its(:solr_field) { is_expected.to eq("series_facet_sim")}
-  its(:solr_values) { is_expected.to eq(["West Campus", "SNCC and Civil Rights Movement"])}
-  its(:solr_params) { is_expected.to eq({"f" => {"series_facet_sim" => ["West Campus", "SNCC and Civil Rights Movement"]}, "sort"=>"title_ssi asc"}) }
   its(:related_documents) { is_expected.to eq([doc2, doc3]) }
+  its(:solr_query) { is_expected.to eq({"f" => {"series_facet_sim" => ["West Campus", "SNCC and Civil Rights Movement"]}, "sort"=>"title_ssi asc"}) }
 end
