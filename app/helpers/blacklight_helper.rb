0c6f9612fd9448a700d67e737f96d20f36e93cd7
Cory Lown <cory.lown@duke.edu>
2017-03-06 14:21:48 -0500

    Adjust metadata list style by length, count of values

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -31,4 +31,69 @@ module BlacklightHelper
     body_class
   end
 
+
+  def presenter_class
+    DdrPublicDocumentPresenter
+  end
+
+  class DdrPublicDocumentPresenter < Blacklight::DocumentPresenter
+
+    ##
+    # Render the show field value for a document
+    #
+    #   Allow an extention point where information in the document
+    #   may drive the value of the field
+    #   @param [String] field
+    #   @param [Hash] options
+    #   @options opts [String] :value
+    def render_document_show_field_value field, options={}
+      field_config = @configuration.show_fields[field]
+      value = options[:value] || get_field_values(field, field_config, options)
+
+      plain_text_value = get_field_values(field, nil, {})
+
+      render_show_field_value value, field_config, plain_text_value
+    end
+
+    ##
+    # Render a value (or array of values) from a field
+    #
+    # @param [String] value or list of values to display
+    # @param [Blacklight::Solr::Configuration::Field] solr field configuration
+    # @param [String] value or list of plain text values
+    # @return [String]
+    def render_show_field_value value=nil, field_config=nil, plain_text_value=nil
+      safe_values = Array(value).collect { |x| x.respond_to?(:force_encoding) ? x.force_encoding("UTF-8") : x }
+
+      if field_config and field_config.itemprop
+        safe_values = safe_values.map { |x| content_tag :span, x, :itemprop => field_config.itemprop }
+      end
+
+      wrap_values(plain_text_value, safe_values)
+    end
+
+    def wrap_values plain_text_value, safe_values
+      plain_value = Array(plain_text_value)
+      value_lengths = plain_value.map { |x| x.length }
+      count = plain_value.length
+      average_length = value_lengths.inject { |sum, x| sum + x }.to_f / count
+
+      if average_length > 150
+        metadata_list_content_tags(safe_values, "long-metadata-values")
+      elsif count >= 7
+        metadata_list_content_tags(safe_values, "many-metadata-values")
+      elsif count <= 6
+        metadata_list_content_tags(safe_values, "few-metadata-values")
+      end
+    end
+
+    def metadata_list_content_tags safe_values, ul_class
+      unless safe_values.blank?
+        safe_values = safe_values.map { |x| content_tag(:li, x) }.join.html_safe
+        content_tag(:ul, safe_values, class: ul_class).html_safe
+      end
+    end
+
+  end
+
 end
--- spec/helpers/blacklight_helper_spec.rb
+++ spec/helpers/blacklight_helper_spec.rb
@@ -25,4 +25,98 @@ RSpec.describe BlacklightHelper do
     end
   end
 
+  describe BlacklightHelper::DdrPublicDocumentPresenter do
+
+    let(:request_context) { double(:add_facet_params => '') }
+    let(:config) { Blacklight::Configuration.new }
+
+    subject { presenter }
+    let(:presenter) { BlacklightHelper::DdrPublicDocumentPresenter.new(document, request_context, config) }
+
+    let(:document) do
+      SolrDocument.new(id: 1, 
+                       'link_to_search_true' => 'x', 
+                       'link_to_search_named' => 'x',
+                       'qwer' => 'document qwer value',
+                       'mnbv' => 'document mnbv value',
+                       'wall_of_text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                       'so_many_subjects' => ["one", "two", "three", "four", "five", "six", "seven"])
+    end
+
+    describe "render_document_show_field_value" do
+      let(:config) do
+        Blacklight::Configuration.new.configure do |config|
+          config.add_show_field 'wall_of_text'
+          config.add_show_field 'so_many_subjects'
+          config.add_show_field 'qwer'
+          config.add_show_field 'asdf', :helper_method => :render_asdf_document_show_field
+          config.add_show_field 'link_to_search_true', :link_to_search => true
+          config.add_show_field 'link_to_search_named', :link_to_search => :some_field
+          config.add_show_field 'highlight', :highlight => true
+        end
+      end
+
+      it "should check for an explicit value" do
+        expect(request_context).to_not receive(:render_asdf_document_show_field)
+        value = subject.render_document_show_field_value 'asdf', :value => 'val1'
+        expect(value).to eq "<ul class=\"few-metadata-values\"><li>val1</li></ul>"
+      end
+
+      it "should check for a helper method to call" do
+        allow(request_context).to receive(:render_asdf_document_show_field).and_return('custom asdf value')
+        value = subject.render_document_show_field_value 'asdf'
+        expect(value).to eq "<ul class=\"few-metadata-values\"><li>custom asdf value</li></ul>"
+      end
+
+      it "should check for a link_to_search" do
+        allow(request_context).to receive(:search_action_path).with('').and_return('/foo')
+        allow(request_context).to receive(:link_to).with("x", '/foo').and_return('bar')
+        value = subject.render_document_show_field_value 'link_to_search_true'
+        expect(value).to eq "<ul class=\"few-metadata-values\"><li>bar</li></ul>"
+      end
+
+      it "should check for a link_to_search with a field name" do
+        allow(request_context).to receive(:search_action_path).with('').and_return('/foo')
+        allow(request_context).to receive(:link_to).with("x", '/foo').and_return('bar')
+        value = subject.render_document_show_field_value 'link_to_search_named'
+        expect(value).to eq "<ul class=\"few-metadata-values\"><li>bar</li></ul>"
+      end
+
+      it "should gracefully handle when no highlight field is available" do
+        allow(document).to receive(:has_highlight_field?).and_return(false)
+        value = subject.render_document_show_field_value 'highlight'
+        expect(value).to be_blank
+      end
+
+      it "should check for a highlighted field" do
+        allow(document).to receive(:has_highlight_field?).and_return(true)
+        allow(document).to receive(:highlight_field).with('highlight').and_return(['<em>highlight</em>'.html_safe])
+        value = subject.render_document_show_field_value 'highlight'
+        expect(value).to eq "<ul class=\"few-metadata-values\"><li><em>highlight</em></li></ul>"
+      end
+
+      it "should add the correct class to a field with few values" do
+        value = subject.render_document_show_field_value 'qwer'
+        expect(value).to eq "<ul class=\"few-metadata-values\"><li>document qwer value</li></ul>"
+      end
+
+      it "should add the correct class to a field with long values" do
+        value = subject.render_document_show_field_value 'wall_of_text'
+        expect(value).to eq "<ul class=\"long-metadata-values\"><li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li></ul>"
+      end
+
+      it "should add the correct class to a field with many values" do
+        value = subject.render_document_show_field_value 'so_many_subjects'
+        expect(value).to eq "<ul class=\"many-metadata-values\"><li>one</li><li>two</li><li>three</li><li>four</li><li>five</li><li>six</li><li>seven</li></ul>"
+      end
+
+      it "should work with show fields that aren't explicitly defined" do
+        value = subject.render_document_show_field_value 'mnbv'
+        expect(value).to eq "<ul class=\"few-metadata-values\"><li>document mnbv value</li></ul>"
+      end
+
+    end
+
+  end
+
 end
