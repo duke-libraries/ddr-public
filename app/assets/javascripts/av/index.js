19e0d72f3813f4b9cfc71b1712382c731e221582
Sean Aery <sean.aery@duke.edu>
2018-10-10 13:38:31 -0400

    Optimize Javascript by refactoring AV, image, & filetree JS tools into compiled assets that only load on pages that use them. Remove unused HLS & DASH providers from JWPlayer to reduce payload. Closes DDR-1498.

---
+++ app/assets/javascripts/av/index.js
@@ -0,0 +1,11 @@
+// This is a manifest file that'll be compiled into av/index.js, which will include all the files
+// listed in the assets/javascripts/av directory, compressed/uglified in production.
+//
+// Audiovisual item JS has been separated from the global application.js so it won't have to load on pages
+// that don't use it.
+// See: https://robots.thoughtbot.com/slicing-up-rails-application-js-for-faster-load-times
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require_tree .
